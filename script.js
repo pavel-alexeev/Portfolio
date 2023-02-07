77d091341af3c7ed5c82686ccf0481c35dd9ac45
Pavels Aleksejevs <paul.alexeev@yahoo.com>
2023-02-07 23:19:35 +0000

    commit

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 function changeText(val) {
   let newText = document.getElementById("text");
+  const textOut = [
+    { transform: "translateX(0)" },
+    { transform: "translateX(-3000px)" },
+  ];
+  const textIn = [
+    { transform: "translateX(1000px)" },
+    { transform: "translateX(0)" },
+    { transition: "ease-out" },
+  ];
+  const timing = {
+    duration: 1000,
+  };
+  document.querySelector("html").style.overflowX = "hidden";
+
   if (val === "shortest") {
+    newText.animate(textIn, timing);
     newText.innerHTML = `I'm a web developer`;
-    newText.style.animation = "1s ease-out";
   }
   if (val === "short-norm") {
+    newText.animate(textIn, timing);
+
     newText.innerHTML = `I'm a web developer and I'm working with JacaScript, HTML/CSS
     to deliver exceptional customer experience.`;
   }
   if (val === "normal") {
+    newText.animate(textIn, timing);
     newText.innerHTML = `I'm a web developer and I'm working with JacaScript, HTML/CSS
     to deliver exceptional customer experience. Adept at
     contributing to a highly collaborative work environment,
